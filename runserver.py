0951869af520c1ccc5e49ac83a1f3e9057ad6cee
partini <partin.imeri@gmail.com>
2014-08-07 17:00:06 +0200

    added the query to return the json for specified room number

--- runserver.py
+++ runserver.py
@@ -42,7 +42,28 @@ def get_observations_for_given_commune(year, election_type, election_round, comm
 	# Return JSON response.
 	return resp
 
+@app.route('/api/kdi/observations/<int:year>/<string:election_type>/<string:election_round>/<string:commune_name>/<string:polling_station_name>/<string:polling_station_room>', methods=['GET'])
+def get_observations_for_given_room_number(year, election_type, election_round, commune_name, polling_station_name,polling_station_room):
+	''' Get observations for given room number.
+	
+	:param: year: The year of the election
+	:param: election_type: The type of the election. Can be local (local-election) or general (general-election)
+	:param: election_round: The round of the election (e.g. firt-round, second-round...)
+	:param: commune_name: The name of the commune.
+	:param: polling_station_name: The name of the polling station.
+	'''
+		
+	# Get the name of the collection we must query on.
+	collection_name = utils.get_collection_name(year, election_type, election_round)
+	
+	# Execute query.
+	observations = mongo.db[collection_name].find({'pollingStation.commune': commune_name, 'pollingStation.name': polling_station_name , 'pollingStation.roomNumber':polling_station_room})
+	
+	# Create JSON response object.
+	resp = Response(response=json_util.dumps(observations), mimetype='application/json')
 
+	# Return JSON response.
+	return resp
 @app.route('/api/kdi/observations/<int:year>/<string:election_type>/<string:election_round>/<string:commune_name>/<string:polling_station_name>', methods=['GET'])
 def get_observations_for_given_polling_station(year, election_type, election_round, commune_name, polling_station_name):
 	''' Get observations for given commune and polling station.
@@ -66,7 +87,39 @@ def get_observations_for_given_polling_station(year, election_type, election_rou
 	# Return JSON response.
 	return resp
 
-
+@app.route('/api/kdi/observations/kvv-members-gender-distribution/<int:year>/<string:election_type>/<string:election_round>/<string:commune_name>/<string:polling_station_name>/<string:room_number>', methods=['GET'])
+def get_kvv_members_gender_distribution_for_given_room(year, election_type, election_round, commune_name,polling_station_name,room_number):
+	''' Get the gender distribution of KVV members in a specified room.
+	
+	:param: year: The year of the election
+	:param: election_type: The type of the election. Can be local (local-election) or general (general-election)
+	:param: election_round: The round of the election (e.g. firt-round, second-round...)
+	:param: commune_name: the name of the commune.
+	'''
+	
+	# Get the name of the collection we must query on.
+	collection_name = utils.get_collection_name(year, election_type, election_round)
+	
+	# Execute query.
+	gender_observation = mongo.db[collection_name].aggregate([
+		{ "$match": 
+			{ "pollingStation.commune":commune_name , "pollingStation.name":polling_station_name ,"pollingStation.roomNumber":room_number}
+		}, 
+		{'$group':
+			{'_id':'$pollingStation.roomNumber',
+			'total':
+				{'$sum':'$preparation.votingMaterialsPlacedInAndOutVotingStation.kvvMembers.total'},
+			'totalFemale':
+				{'$sum':'$preparation.votingMaterialsPlacedInAndOutVotingStation.kvvMembers.female'}
+			}
+		}
+	])
+	
+	# Create JSON response object.
+	resp = Response(response=json_util.dumps(gender_observation), mimetype='application/json')
+	
+	# Return JSON response.
+	return resp
 @app.route('/api/kdi/observations/kvv-members-gender-distribution/<int:year>/<string:election_type>/<string:election_round>/<string:commune_name>', methods=['GET'])
 def get_kvv_members_gender_distribution_for_given_commune(year, election_type, election_round, commune_name):
 	''' Get the gender distribution of KVV members in a specified commune.
@@ -223,10 +276,57 @@ def get_number_of_votes_casted_for_given_polling_station(year, election_type, el
 
 	# Return JSON response.
 	return resp
-@app.route('/kdi/observations/search/<int:year>/<string:election_type>/<string:election_round>/<string:commune>/<string:polling_station>/<string:ultra_violet_control>/<string:finger_sprayed>/<string:query_method>', methods=['GET'])
-def search(year,election_type,election_round,commune,polling_station,ultra_violet_control,finger_sprayed,query_method):
-	''' Get the results for the parameters that are taken from the GET Method.
+
+@app.route('/api/kdi/observations/votes-count/<int:year>/<string:election_type>/<string:election_round>/<string:commune_name>/<string:polling_station_name>/<string:room_number>', methods=['GET'])
+def get_number_of_votes_casted_for_given_room(year, election_type, election_round, commune_name,polling_station_name,room_number):
+	''' Get the number of votes casted in a specified room.
+	The votes are grouped by the following hours: 10 AM, 1 PM, 4 PM, and 7 PM.
 	
+	:param: year: The year of the election
+	:param: election_type: The type of the election. Can be local (local-election) or general (general-election)
+	:param: election_round: The round of the election (e.g. firt-round, second-round...)
+	:param: commune_name: the name of the commune.
+	:param: polling_station_name: the name of the polling station.
+	:param: polling_station_name: the name of the polling station.
+	'''
+	
+	# Get the name of the collection we must query on.
+	collection_name = utils.get_collection_name(year, election_type, election_round)
+	
+	# Execute query.
+	voted_by_observation = mongo.db[collection_name].aggregate([
+			{ "$match":
+				{ "pollingStation.commune":commune_name,"pollingStation.name":polling_station_name,"pollingStation.roomNumber":room_number } 
+			},
+			{'$group':
+				{'_id':'$pollingStation.roomNumber',
+					'tenAM':{
+						'$sum':'$votingProcess.voters.howManyVotedBy.tenAM'
+							},
+					'onePM':{
+						'$sum':'$votingProcess.voters.howManyVotedBy.onePM'
+							},
+					'fourPM':{
+						'$sum':'$votingProcess.voters.howManyVotedBy.fourPM'
+							},
+					'sevenPM':{
+						'$sum':'$votingProcess.voters.howManyVotedBy.sevenPM'
+							}
+				}
+			}
+		])
+	
+	# Create JSON response object.
+	resp = Response(response=json_util.dumps(voted_by_observation), mimetype='application/json')
+	
+	# Return JSON response.
+	return resp
+
+
+@app.route('/kdi/observations/search/<int:year>/<string:election_type>/<string:election_round>/', methods=['GET'])
+def search(year,election_type,election_round):
+	''' Get the results for the parameters that are taken from the GET Method.
+
 	:param: year: The year of the election
 	:param: election_type: The type of the election. Can be local (local-election) or general (general-election)
 	:param: election_round: The round of the election (e.g. firt-round, second-round...)
@@ -236,31 +336,24 @@ def search(year,election_type,election_round,commune,polling_station,ultra_viole
 	:param: finger_sprayed: checks if finger spray happened in that polling station
 	:param:	query_method: the type of the query that is going to hit MongoDB,  AND/OR
 	'''
+	commune = request.args.get('commune')
+	polling_station = request.args.get('pollingStation')
+	ultra_violet_control = request.args.get('ultraVioletControl')
+	finger_sprayed = request.args.get('fingerSprayed')
+
 	# Get the name of the collection we must query on.
 	collection_name = utils.get_collection_name(year, election_type, election_round)
 	# Execute query.
 	# If the query_method is equals to "and", execute mongo query with $and operator
-	if(query_method == "and"):
-		search_results_and = mongo.db[collection_name].find({ 
-																"$and" : [ 
-																	{"pollingStation.commune" : commune },
-																	{"pollingStation.name" : polling_station},
-																	{"votingProcess.voters.ultraVioletControl" : ultra_violet_control},
-																	{"votingProcess.voters.fingerSprayed" : finger_sprayed }
-																]})
-		resp = Response(response = json_util.dumps(search_results_and), mimetype = 'application/json')
-	# If the query_method is equals to "or", execute mongo query with $or operator
-	elif( query_method == "or"):
-		search_results_or = mongo.db[collection_name].find({ 
-																"$or" : [ 
-																	{"pollingStation.commune" : commune },
-																	{"pollingStation.name" : polling_station},
-																	{"votingProcess.voters.ultraVioletControl" : ultra_violet_control},
-																	{"votingProcess.voters.fingerSprayed" : finger_sprayed }
-																]})
-		resp = Response(response = json_util.dumps(search_results_or), mimetype = 'application/json')
 	
-		
+	search_results_and = mongo.db[collection_name].find({ 
+															"$and" : [ 
+																{"pollingStation.commune" : commune },
+																{"pollingStation.name" : polling_station},
+																{"votingProcess.voters.ultraVioletControl" : ultra_violet_control},
+																{"votingProcess.voters.fingerSprayed" : finger_sprayed }
+															]})
+	resp = Response(response = json_util.dumps(search_results_and), mimetype = 'application/json')
 
 	# Return JSON response.
 	return resp
