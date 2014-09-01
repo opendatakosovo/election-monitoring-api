f65fcd68891a2218b69c256459c4449e8aaa7e1a
legeorges <georges.labreche@gmail.com>
2014-09-01 03:51:53 -0700

    Load base path config

--- ema/__init__.py
+++ ema/__init__.py
@@ -58,7 +58,7 @@ def load_config(app):
 
 	# Set up config properties
 	app.config['SERVER_PORT'] = config.get('Application', 'SERVER_PORT')
-	app.config['BASE_PATH'] = config.get('Mongo', 'BASE_PATH')
+	app.config['BASE_PATH'] = config.get('Application', 'BASE_PATH')
 
 	app.config['MONGO_DBNAME'] = config.get('Mongo', 'DB_NAME')
 
